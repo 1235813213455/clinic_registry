cc8efd62e828359d777ebf5d3ab012b8dd88ecde
asd <asd@nosuchserver.asd>
2015-09-16 21:57:16 +0400

    customizing and tweaking admin ui

--- clinic_registry/registry/admin.py
+++ clinic_registry/registry/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Doctor, Patient, Assignment
+
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'date', 'time_string')
+    list_filter = (('date', DateFieldListFilter), )
+    search_fields = ('doctor__name', )
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Assignment, AssignmentAdmin)
--- clinic_registry/registry/models.py
+++ clinic_registry/registry/models.py
@@ -13,27 +13,34 @@ class Patient(models.Model):
     father_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.first_name + " " + self.second_name \
+        return self.second_name + " " + self.first_name \
                + " " + self.father_name
 
 class Assignment(models.Model):
-    HOUR_1 = {"id":9, "range":"9:00 - 10:00"} 
-    HOUR_2 = {"id":10, "range":"10:00 - 11:00"} 
-    HOUR_3 = {"id":11, "range":"11:00 - 12:00"}
-    HOUR_4 = {"id":12, "range":"12:00 - 13:00"}
-    HOUR_5 = {"id":13, "range":"13:00 - 14:00"}
-    HOUR_6 = {"id":14, "range":"14:00 - 15:00"}
-    HOUR_7 = {"id":15, "range":"15:00 - 16:00"}
-    HOUR_8 = {"id":16, "range":"16:00 - 17:00"}
-    HOUR_9 = {"id":17, "range":"17:00 - 18:00"}
-
-    HOUR_CHOICES = (HOUR_1, HOUR_2, HOUR_3, HOUR_4, HOUR_5,
-                    HOUR_6, HOUR_7, HOUR_8, HOUR_9)
+    HOUR_MAP = {9: {"id":9, "range":"9:00 - 10:00"}, 
+                10: {"id":10, "range":"10:00 - 11:00"}, 
+                11: {"id":11, "range":"11:00 - 12:00"},
+                12: {"id":12, "range":"12:00 - 13:00"},
+                13: {"id":13, "range":"13:00 - 14:00"},
+                14: {"id":14, "range":"14:00 - 15:00"},
+                15: {"id":15, "range":"15:00 - 16:00"},
+                16: {"id":16, "range":"16:00 - 17:00"},
+                17: {"id":17, "range":"17:00 - 18:00"}}
+    HOUR_CHOICES = HOUR_MAP.values()
 
     patient = models.ForeignKey(Patient)
     doctor = models.ForeignKey(Doctor)
-    time = models.IntegerField(default=HOUR_1["id"])
+    time = models.IntegerField(default=HOUR_MAP[9]["id"], 
+                               choices=[(k, v["range"]) for k, v in HOUR_MAP.items()])
     date = models.DateField()
 
+
+    def time_string(self):
+        return Assignment.HOUR_MAP[self.time]["range"] 
+
+    time_string.short_description = "Time"
+
+
     def __str__(self):
-        return "%s: %s (%s, %s)" % (self.doctor, self.patient, date, time)
+        assignment_time = Assignment.HOUR_MAP[self.time]
+        return "%s: %s (%s, %s)" % (self.doctor, self.patient, str(self.date), assignment_time["range"])
